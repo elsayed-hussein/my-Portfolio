16db4bee0e266c49a427afcf634b6c80db80cb2b
elsayed-hussein <elsayed.hussen@gmail.com>
2022-12-01 13:51:40 +0200

    part one

--- code/assets/js/index.js
+++ code/assets/js/index.js
@@ -1,2 +1,8 @@
 "use strict";
+let typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+});
 //# sourceMappingURL=index.js.map
---
+++ code/assets/ts/index.ts
@@ -0,0 +1,12 @@
+// Variables Start
+
+// / Variables End
+/* ================================================== */
+// Typing Animation Start
+let typed = new Typed(".typing", {
+  strings: ["Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+// / Typing Animation End
